6cb3cbf05fe0eb61211679265c3363738645f22d
eline-91 <elinevanelburg@hotmail.com>
2016-01-22 01:07:41 +0100

    Fixed errors and delivered working code to create a shapefile.

--- WebMapping.py
+++ WebMapping.py
@@ -2,17 +2,6 @@
 # Web Mapping demo
 # 21-01-2016
 
-# Just in case
-list_in_case = [['Wageningen', [51.9691868, 5.6653948]],
-                ['Amsterdam', [52.3702157, 4.895167900000001]],
-                ['Fuji', [35.3605555, 138.7277777]],
-                ['Lima', [-12.046374, -77.0427934]],
-                ['Bennekom', [51.99910389999999, 5.674754399999999]],
-                ['Cairo', [30.0444196, 31.2357116]],
-                ['Sao Paulo', [-23.5505199, -46.63330939999999]],
-                ['Brisbane', [-27.4710107, 153.0234489]],
-                ['New York', [40.7127837, -74.0059413]]]
-
 # Imports
 import geocoder
 import osgeo.ogr, osgeo.osr
@@ -34,33 +23,40 @@ def get_location(city):
     latlong = g.latlng
     return latlong
 
-def make_shp(locationList):
+def make_shp(locationList, fileName):
     # Set spatial reference
     spatialReference = osgeo.osr.SpatialReference()
     spatialReference.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
 
     # Create shapefile
     driver = osgeo.ogr.GetDriverByName('ESRI Shapefile')
-    fileName = 'CityLocation_' + time.strftime('%Y%m%d_%H%M_%S') + '.shp'
     shapeData = driver.CreateDataSource(fileName)
 
     # Create Layer
     layer = shapeData.CreateLayer('cities', spatialReference, osgeo.ogr.wkbPoint)
     layerDefinition = layer.GetLayerDefn()
 
-    i = 1
+    # Create Name field
+    fid_field = osgeo.ogr.FieldDefn("FID", osgeo.ogr.OFTInteger)
+    name_field = osgeo.ogr.FieldDefn("PlaceNames", osgeo.ogr.OFTString)
+    name_field.SetWidth(30)
+    layer.CreateField(fid_field)
+    layer.CreateField(name_field)
+
     # Create Points
+    i = 1
     for city_loc in locationList:
         city = city_loc[0]
         coords = city_loc[1]
         print coords
         # Create point
         geometry = osgeo.ogr.Geometry(osgeo.ogr.wkbPoint)
-        geometry.SetPoint(0, coords[0], coords[1])
+        geometry.SetPoint(0, coords[1], coords[0])
         # Create feature
         feature = osgeo.ogr.Feature(layerDefinition)
         feature.SetGeometry(geometry)
-        feature.SetFID(i)
+        feature.SetField("FID",i)
+        feature.SetField("PlaceNames",city)
         # Save feature
         layer.CreateFeature(feature)
         # Cleanup
@@ -71,7 +67,7 @@ def make_shp(locationList):
     shapeData.Destroy()
         
 # Main code
-string_cities = read_file("C:/Users/Eline/Documents/Python/Geo-Scripting/WebMapping/cities.txt")
+string_cities = read_file("/home/eline/Documents/Python/Projects/Web_Mapping/WebMapping/cities.txt")
 list_cities = split_string(string_cities)
 
 location_list = []
@@ -79,4 +75,5 @@ for city in list_cities:
     location = get_location(city)
     location_list.append([city,location])
 
-make_shp(location_list)
+shp_filename = '/home/eline/Documents/Python/Projects/Web_Mapping/WebMapping/Shapefiles/CityLocation_' + time.strftime('%Y%m%d_%H%M') + '.shp'
+make_shp(location_list, shp_filename)
